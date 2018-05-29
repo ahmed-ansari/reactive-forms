1d2d40dcc5e824c50dd32569559aad17c4b698ea
Ahmed Ansari <ansari@Srinivass-Mac-miAni-2.local>
2018-05-29 12:52:01 +0530

    Changes made for Reactive form Integration (formGroup and formControlName) and commented template driven form changes

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit {
     customer: Customer= new Customer();
+    customerForm: FormGroup;
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    ngOnInit(): void {
+        this.customerForm =  new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true),
+        })
+    }
+    // save(customerForm: NgForm) {
+    save() {
+        console.log(this.customer);
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+    testData() {
+        // this.customerForm.setValue({
+        //     firstName:"Ansari",
+        //     lastName:"Ahmed",
+        //     sendCatalog:true,
+        //     email:"ansari@gmail.com"
+        // })
+
+        this.customerForm.patchValue({
+            firstName:"Ansari",
+            // lastName:"Ahmed",
+            sendCatalog:true,
+            email:"ansari@gmail.com"
+        })
     }
  }
--- readme.md
+++ readme.md
@@ -1,8 +1,2 @@
-1) Open a command prompt in the project's root directory
-
-2) Type: `npm install`
-    This installs the dependencies as defined in the package.json file.
-    
-3) Type: `npm start`
-    This launches the TypeScript compiler (tsc) to compile the application and wait for changes. 
-    It also starts the lite-server and launches the browser to run the application.
+# reactive-forms
+Angular 4 Reactive form (considering both template and reactive forms)
